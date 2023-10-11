a1c84218180525476591448e0027d9788f22166a
Tim Collins <tim@thecollins.team>
2023-10-11 09:48:54 +0100

    appease some linty things

--- examples/get-versions/workflow.py
+++ examples/get-versions/workflow.py
@@ -1,41 +1,49 @@
-from hera.workflows import (
-    DAG,
-    Container,
-    Parameter,
-    Workflow,
-)
-from pipekit_sdk.service import PipekitService
 import os
 
+from hera.workflows import DAG, Container, Parameter, Workflow
+from pipekit_sdk.service import PipekitService
+
 # Obtain Pipekit Hera token from environment variable
-pipekit = PipekitService(token=os.environ['PIPEKIT_HERA_TOKEN'])
+pipekit = PipekitService(token=os.environ["PIPEKIT_HERA_TOKEN"])
 
 items = [
-    {"label": "app.kubernetes.io/component=workflow-controller", "hrname": "Argo Workflows", "type": "deployment"},
-    {"label": "app.kubernetes.io/name=pipekit-agent", "hrname": "Pipekit Agent", "type": "deployment"},
-    {"label": "app=minio", "hrname": "Minio", "type": "statefulset"}
+    {
+        "label": "app.kubernetes.io/component=workflow-controller",
+        "hrname": "Argo Workflows",
+        "type": "deployment",
+    },
+    {
+        "label": "app.kubernetes.io/name=pipekit-agent",
+        "hrname": "Pipekit Agent",
+        "type": "deployment",
+    },
+    {"label": "app=minio", "hrname": "Minio", "type": "statefulset"},
 ]
 
 with Workflow(
     generate_name="versions-",
     entrypoint="main",
     namespace="argo",
-    service_account_name="argo-workflow"
+    service_account_name="argo-workflow",
 ) as w:
     kubernetes_version = Container(
         name="kubernetes-version",
         image="dtzar/helm-kubectl",
-        command=["/bin/bash", "-c", "kubectl version"]
+        command=["/bin/bash", "-c", "kubectl version"],
     )
 
     kubectl_versions = Container(
         name="kubectl-version",
         image="dtzar/helm-kubectl",
-        command=["/bin/bash", "-c", "version=$(kubectl -n pipekit get {{inputs.parameters.type}} -l {{inputs.parameters.label}} -o=jsonpath=\"{..image}\") && echo \"{{inputs.parameters.hrname}}: $version\""],
+        command=[
+            "/bin/bash",
+            "-c",
+            'version=$(kubectl -n pipekit get {{inputs.parameters.type}} -l {{inputs.parameters.label}} -o=jsonpath="{..image}") && echo "{{inputs.parameters.hrname}}: $version"',
+        ],
         inputs=[
             Parameter(name="label"),
             Parameter(name="hrname"),
-            Parameter(name="type")
+            Parameter(name="type"),
         ],
     )
 
@@ -58,4 +66,6 @@ pipe_run = pipekit.submit(w, "free-trial-cluster")
 
 # Print Run URL
 run_info = pipekit.get_run(pipe_run["uuid"])
-print (f"Observe the run at: https://pipekit.io/pipes/{run_info['pipeUUID']}/runs/{run_info['uuid']}")
+print(
+    f"Observe the run at: https://pipekit.io/pipes/{run_info['pipeUUID']}/runs/{run_info['uuid']}"
+)
--- examples/hera-coinflip/workflow.py
+++ examples/hera-coinflip/workflow.py
@@ -1,10 +1,11 @@
+import os
+
 from hera.workflows import DAG, Workflow, script
-from hera.shared import global_config
 from pipekit_sdk.service import PipekitService
 
-import os
 # Obtain Pipekit Hera token from environment variable
-pipekit = PipekitService(token=os.environ['PIPEKIT_HERA_TOKEN'])
+pipekit = PipekitService(token=os.environ["PIPEKIT_HERA_TOKEN"])
+
 
 @script()
 def flip():
@@ -13,15 +14,23 @@ def flip():
     result = "heads" if random.randint(0, 1) == 0 else "tails"
     print(result)
 
+
 @script()
 def heads():
     print("it was heads")
+
 
 @script()
 def tails():
     print("it was tails")
 
-with Workflow(generate_name="coinflip-", entrypoint="d", namespace="argo", service_account_name="argo-workflow") as w:
+
+with Workflow(
+    generate_name="coinflip-",
+    entrypoint="d",
+    namespace="argo",
+    service_account_name="argo-workflow",
+) as w:
     with DAG(name="d") as s:
         f = flip()
         heads().on_other_result(f, "heads")
@@ -35,4 +44,6 @@ pipe_run = pipekit.submit(w, "free-trial-cluster")
 
 # Print Run URL
 run_info = pipekit.get_run(pipe_run["uuid"])
-print (f"Observe the run at: https://pipekit.io/pipes/{run_info['pipeUUID']}/runs/{run_info['uuid']}")
+print(
+    f"Observe the run at: https://pipekit.io/pipes/{run_info['pipeUUID']}/runs/{run_info['uuid']}"
+)
